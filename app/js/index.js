00d57d305d0b78c4fd49b0f33716abcde50ccfc4
gyoon <woosys25@gmail.com>
2017-03-11 04:39:23 +0900

    init

--- app/js/index.js
+++ app/js/index.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
+
     $('#fullpage').fullpage();
+
+
+    $('.autoplay').slick({
+        slidesToShow: 3,
+        //dots: true,
+        slidesToScroll: 1,
+        autoplay: false,
+    });
+
+
 });
+
+
